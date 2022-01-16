f5b460128b00da57f0e7b88d484c737d947495c6
ShadowN0 <ShadowNN0000@gmail.com>
2022-01-16 18:27:20 +0100

    added full functionality

--- snake.js
+++ snake.js
@@ -6,8 +6,12 @@ class Board {
         this.tiles_on_board = [] // divs that are representation of fields array
         this.snake = []
         this.snake_head = 0
-        this.apples
-    
+        this.apples = [] //indexes of apples
+        this.not_spawned_apples = [] //indexes of not spawned apples
+        this.mv_direction_interval
+        this.active_direction
+        this.eat  // should be equal to '= this.eat_apple()'
+        this.stop_apple_spawn = 0
     }
 
     generate_field(){
@@ -27,8 +31,10 @@ class Board {
         this.fields_array = this.generate_field()
     }
 
-    //render field to the screen
+    
     render(){
+        
+        //render field to the screen
         for(let row = 0; row < this.height; row++){
             let row_div = document.createElement('div')
             
@@ -50,7 +56,7 @@ class Board {
         this.snake.push(position)
 
         let head = this.tiles_on_board[position]
-        head.style.backgroundColor = 'lime'
+        head.style.backgroundColor = 'violet'
 
     }
     
@@ -66,77 +72,255 @@ class Board {
     }
 
     refresh_snake(){
-        let to_remove = this.snake
-        console.log(to_remove, "sadjsj", this.snake.length)
 
-
-        this.tiles_on_board[this.snake[this.snake.length - 1]].style.backgroundColor = 'wheat'
+        //console.log(this.tiles_on_board[this.snake[this.snake.length - 1]])
+        //takes the last colored tile, which number is stored in last index of this.snake, and changes it to background color
+        this.tiles_on_board[this.snake[this.snake.length - 1]].style.backgroundColor = 'rgb(11, 77, 6)'
+        //removes the last element of snake, because it's no longer a part of it
         this.snake.pop()
     }
 
     render_snake(){
         let head_pos = this.snake_head
 
+        //moves snake head to a newer position
         this.snake.unshift(head_pos)
-
-        this.tiles_on_board[this.snake[0]].style.backgroundColor = 'lime'
+        this.tiles_on_board[this.snake[0]].style.backgroundColor = 'violet'
     }
     
     handle_keypress(board){
         document.addEventListener('keypress', function(e){
             let _this = board
             let key = e.code
-            console.log(key)
+            //console.log(key)
             
-            switch(key){
-                case 'KeyA':
-                    _this.refresh_snake()
-                    _this.snake_head -= 1 
-                    _this.render_snake()
-                    break
-
-                case 'KeyS':
-                    _this.refresh_snake()
-                    _this.snake_head += _this.width
-                    _this.render_snake()
-                    break
-
-                case 'KeyD':
-                    _this.refresh_snake()
-                    _this.snake_head += 1
-                    _this.render_snake()
-                    break
-
-                case 'KeyW':
-                    _this.refresh_snake()
-                    console.log(_this.snake_head)
-                    _this.snake_head -= _this.width
-                    console.log(_this.snake_head)
-                    _this.render_snake()
-                    break
-
-                default:
-                    console.log('bruh')
-            }
+            //this handles all of the movement and inputs
+            _this.movement_handler(_this, key)
         })
     }
 
+    movement_handler(_this, key){
+        
+        if(key != _this.active_direction){
+            clearInterval(_this.mv_direction_interval)
+            _this.movement_direction_set(key, _this)
+        } 
+    }
+
+    movement_direction_set(key, _this){
+        switch(key){
+            case 'KeyA':
+                //it's just _this.refresh_and_render_snake(_this, -1) done every 1 second                
+                _this.active_direction = 'KeyA'
+                _this.mv_direction_interval = setInterval(_this.refresh_and_render_snake, 200, _this, -1)
+                
+                break
+
+            case 'KeyS':  
+                //it's just _this.refresh_and_render_snake(_this, _this.width) done every 1 second                
+                _this.active_direction = 'KeyS'
+                _this.mv_direction_interval = setInterval(_this.refresh_and_render_snake, 200, _this, _this.width)
+       
+                break
+
+            case 'KeyD':
+                 //it's just _this.refresh_and_render_snake(_this, 1) done every 1 second                
+                 _this.active_direction = 'KeyD'
+                _this.mv_direction_interval = setInterval(_this.refresh_and_render_snake, 200, _this, 1)
+            
+                break
+
+            case 'KeyW':
+                _this.active_direction = 'KeyW'
+                _this.mv_direction_interval = setInterval(_this.refresh_and_render_snake, 200, _this, -_this.width)
+
+                break
+                
+                default:
+                //it's just _this.refresh_and_render_snake(_this, -_this.width) done every 1 second   
+                clearInterval(_this.mv_direction_interval)             
+                console.log('bruh')
+        }
+    }
+
+    refresh_and_render_snake(_this, snake_head_pos_offset){
+
+        _this.eat_apple()
+        let new_snake_element = _this.snake_head
+        if(_this.eat == true){   
+            //push new element to the snake array
+            _this.snake.push(new_snake_element)
+            
+            //offset the new head position and render it on screen
+            if(_this.detect_collision(_this.snake_head + snake_head_pos_offset) == false){
+                clearInterval(_this.mv_direction_interval)
+                alert('zajebałeś mordą w ścianę')
+                _this.handle_keypress = 0
+                _this.movement_handler = function(){
+                    console.log('you lost, stop trying')
+                }
+                _this.stop_apple_spawn = 1 
+                return
+            }
+
+            _this.snake_head += snake_head_pos_offset
+            _this.render_snake()
+            
+            //delete last element witout coloring to prevent visual bugs
+            _this.snake.pop()
+        }
+        else{
+            //delete last element and decolor it, and then offset new_head, add it to array and color tile of it's index
+            _this.refresh_snake()
+
+            if(_this.detect_collision(_this.snake_head + snake_head_pos_offset) == false){
+                clearInterval(_this.mv_direction_interval)
+                alert('zajebałeś mordą w ścianę')
+                _this.handle_keypress = 0
+                _this.movement_handler = function(){
+                    console.log('you lost, stop trying')
+                }
+                _this.stop_apple_spawn = 1 
+                return
+            }
+
+            _this.snake_head += snake_head_pos_offset
+            _this.render_snake()
+        }
+    }
+
+    eat_apple(){
+        let index_of_eating = this.apples.indexOf(this.snake_head)
+        console.log(index_of_eating, this.apples[index_of_eating])
+        //it means that there is a collision between head and apple at index == index_of_eating
+        if(index_of_eating != -1){
+            //console.log(_this.apples)
+            
+            this.apples.splice(index_of_eating, 1)
+            //console.log(_this.apples)
+            this.eat =  true
+            return
+        }
+
+        this.eat = false
+    }
+
     spawn_apple(){
+        
+        //search for empty spot on map and after that add this position to apples array
+        let search_for_spot = true
+        let cords 
+        let position
+
+        while(search_for_spot){
+            cords = this.randomize_start_position()
+            position = cords[0] + cords[1] * this.width
+
+            if(this.apples.length == this.tiles_on_board.length - this.snake.length){
+                //console.log(this.apples.length, this.tiles_on_board.length, this.snake.length)
+                break
+            }
+
+            if(this.snake.indexOf(position) == -1 && this.apples.indexOf(position) == -1){
+                search_for_spot = false 
+                this.apples.push(position)
+                this.not_spawned_apples.push(position)
+                //console.log('added apple index')
+            }
+    
+        }
+        
+        
+    }
+
+    render_apple(_this){
+        if(_this.stop_apple_spawn ==  1){
+            return
+        }
+        //object must be passed, because of the change of context (sees this as a window)
+
+        //adds apple's position to this.apple property
+        _this.spawn_apple()
+        
+        //this colors tile which corresponds to the apple index
+        if(_this.not_spawned_apples.length != 0){
+            //console.log('apple spawned!')
+            let last_tile = _this.not_spawned_apples[_this.not_spawned_apples.length - 1]
+            
+            _this.tiles_on_board[last_tile].style.backgroundColor = 'crimson'
+            _this.not_spawned_apples.pop()
+            
+        }
+        else{
+            //console.log('there is no apple to spawn')
+        }
 
     }
+
+    // set_collision_with_map(){
+    //     let borders = []
+
+    //     let border_top = []
+    //     let border_bottom = []
+    //     let border_right = []
+    //     let border_left = []
+    //     //top bottom right left
+        
+    //     //append top and bottom rows to borders[]
+    //     for(let row = 0; row < this.width; row++){
+    //         border_top.push(row)
+    //         border_bottom.push(row + (this.height - 1) * this.width)
+    //     }
+        
+    //     for(let col = 0; col < this.height; col++){
+    //         border_left.push(col * this.width)
+    //         border_right.push(col * this.width + this.width - 1)
+    //     }
+
+    //     borders.push(border_top, border_bottom, border_right, border_left)
+    //     this.borders = borders
+    // }
+
+    detect_collision(future_head_pos){
+       
+        if(future_head_pos >= this.width*this.height  || future_head_pos < 0){
+            return false
+        }
+
+        if(((future_head_pos%10 == this.snake_head%10 + this.width - 1) || (future_head_pos%10 == this.snake_head%10 - this.width + 1)) && (future_head_pos == this.snake_head+1 || future_head_pos == this.snake_head-1)){
+            console.log(future_head_pos, this.snake_head)
+            return false
+        }
+
+        if(this.snake.indexOf(future_head_pos) != -1){
+            return false
+        }
+        
+        return true
+    }
+
 }
 
 
 let game_div = document.getElementById('game_div')
 
 //create instance of a game
-let board = new Board(10,10)
+let board = new Board(10, 10)
 console.log(board)
 
 //make array of zeroes and render it to screen
 board.set_arr_field()
 board.render()
-board.create_snake_head()
-board.handle_keypress(board)
 console.table(board.fields_array)
 
+//sets this.borders
+//board.set_collision_with_map()
+
+//randomize starting position
+board.create_snake_head()
+
+//keypress handler
+board.handle_keypress(board)
+
+//spawns and renders apple once every x time
+let spawner = setInterval(board.render_apple, 1000, board)
